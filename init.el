f95ba5f4373ea3fdc6a59d1838a1e42cf2950ede
Cullen Grover <cgrover@redhat.com>
2025-02-28 16:11:27 -0500

    README and initial init.el file

---
+++ init.el
@@ -0,0 +1,21 @@
+;;; init.el --- Init! -*- lexical-binding: t -*-
+;;; Commentary:
+
+;;  My custom init file
+
+;;; Code:
+
+;; Customize-generated Lisp...
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(leuven-dark)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
