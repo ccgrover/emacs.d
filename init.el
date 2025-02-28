2225cb0403c55ede90a7e3bda7b5b2680e242d30
Cullen Grover <cgrover@redhat.com>
2025-02-28 16:26:48 -0500

    Initial package management; add roadmap

--- init.el
+++ init.el
@@ -5,6 +5,32 @@
 
 ;;; Code:
 
+;;  Set up package management
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("gnu"   . "https://elpa.gnu.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package-ensure)
+(setq use-package-always-ensure t
+      use-package-expand-minimally t)
+
+;; backup behavior
+
+(setq backup-directory-alist `(("." . "~/.emacs_file_backups")))
+(setq backup-by-copying-when-linked t)
+
+;; git
+
+(use-package magit)
+
 ;; Customize-generated Lisp...
 
 (custom-set-variables
@@ -12,7 +38,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(leuven-dark)))
+ '(custom-enabled-themes '(leuven-dark))
+ '(package-selected-packages '(magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
