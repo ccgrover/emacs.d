f7111322f4662c2cb30bb0f46b302ae9c10cf604
Cullen Grover <cgrover@redhat.com>
2025-03-25 16:21:49 -0400

    Ported cullen-lsp-*

--- modules/cullen-lsp.el
+++ modules/cullen-lsp.el
@@ -6,41 +6,29 @@
 
 ;;; Code:
 
-(condition-case nil
-    (require 'use-package)
-  (file-error
-   (require 'package)
-   (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-   (package-initialize)
-   (package-refresh-contents)
-   (package-install 'use-package)
-   (setq use-package-always-ensure t)
-   (require 'use-package)))
-
-(setq use-package-always-ensure t)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-
 (use-package projectile)
-(use-package flycheck)
+(use-package flycheck
+  :hook (flycheck-mode . flycheck-set-indication-mode)
+  :custom (flycheck-indication-mode 'left-margin))
 (use-package yasnippet
-  :config (yas-global-mode))
+  :commands yas-global-mode)
 (use-package lsp-mode
-  :hook ((lsp-mode . lsp-enable-which-key-integration)))
+  :hook (lsp-mode . lsp-enable-which-key-integration))
 (use-package hydra)
 (use-package company)
 (use-package lsp-ui)
 (use-package which-key
-  :config (which-key-mode))
+  :commands which-key-mode)
 (use-package lsp-java
-  :config (add-hook 'java-mode-hook 'lsp))
+  :hook (java-mode . lsp))
 (use-package dap-mode
   :after lsp-mode
-  :config (dap-auto-configure-mode))
+  :commands dap-auto-configure-mode)
 (use-package dap-java
   :ensure nil)
 (use-package helm-lsp)
 (use-package helm
-  :config (helm-mode))
+  :commands helm-mode)
 (use-package lsp-treemacs)
 
 (provide 'cullen-lsp)
