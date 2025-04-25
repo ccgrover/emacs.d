557fbf941469e2a089d647b93f7f8d2b85407cac
Cullen Grover <cgrover@redhat.com>
2025-04-25 12:55:03 -0400

    Add org roam capture template for pages

--- modules/my-org.el
+++ modules/my-org.el
@@ -20,14 +20,20 @@
   :config (org-babel-do-load-languages
            'org-babel-load-languages
            '((plantuml . t))) ; this line activates plantuml
-  :config (define-prefix-command 'my-org-mode-map)
+  :config
+  (define-prefix-command 'my-org-mode-map)
   (keymap-set 'my-org-mode-map "a" #'org-agenda)
   (keymap-set 'my-org-mode-map "l" #'org-todo-list)
   (keymap-set 'my-org-mode-map "c" #'org-capture)
   (keymap-global-set "C-c o" 'my-org-mode-map))
 
 (use-package org-roam
-  :custom (org-roam-directory my-org-directory))
+  :custom (org-roam-directory my-org-directory)
+  :config
+  (setq org-roam-capture-templates
+        '(("p" "Generic pages" plain
+           (file "/home/cgrover/.emacs.d/templates/org-roam/pages.org")
+           :target (file "pages/${slug}.org")))))
 
 (provide 'my-org)
 ;;; my-org.el ends here
