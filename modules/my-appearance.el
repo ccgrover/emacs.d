5ad692b3a1723dcc04c2a037c9e70545b3937f59
Cullen Grover <cullencgrover@gmail.com>
2025-11-09 17:16:34 -0600

    Fix themes

--- modules/my-appearance.el
+++ modules/my-appearance.el
@@ -52,11 +52,28 @@
 
 (use-package ef-themes
   :ensure t
-  :defer nil ;; necessary since ":bind" implies "defer t"
-  :bind ("<f7>" . ef-themes-rotate)
-  :bind ("<f8>" . ef-themes-toggle)
+  :init
+  ;; This makes the Modus commands listed below consider only the Ef
+  ;; themes.  For an alternative that includes Modus and all
+  ;; derivative themes (like Ef), enable the
+  ;; `modus-themes-include-derivatives-mode' instead.  The manual of
+  ;; the Ef themes has a section that explains all the possibilities:
+  ;;
+  ;; - Evaluate `(info "(ef-themes) Working with other Modus themes or taking over Modus")'
+  ;; - Visit <https://protesilaos.com/emacs/ef-themes#h:6585235a-5219-4f78-9dd5-6a64d87d1b6e>
+  (ef-themes-take-over-modus-themes-mode 1)
+  :bind (("<f7>" . ef-themes-rotate)
+         ("<f8>" . ef-themes-toggle))
+
   :config
-  (ef-themes-select 'ef-autumn))
+  ;; All customisations here.
+  (setq modus-themes-mixed-fonts t)
+  (setq modus-themes-italic-constructs t)
+
+  ;; Finally, load your theme of choice (or a random one with
+  ;; `modus-themes-load-random', `modus-themes-load-random-dark',
+  ;; `modus-themes-load-random-light').
+  (modus-themes-load-theme 'ef-autumn))
 
 ;; https://skybert.net/emacs/simple-modeline-brings-peace-of-mind/
 
